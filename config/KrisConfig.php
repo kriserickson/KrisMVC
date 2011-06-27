2b4e6ec1754c9a70950ec7c7c862f89c4a5231a3
Kris Erickson <ksoncan34@yahoo.ca>
2011-06-26 22:04:32 -0700

    Refactoring the config module.

--- config/KrisConfig.php
+++ config/KrisConfig.php
@@ -3,16 +3,18 @@
 class KrisConfig
 {
     // Change Config options here... TODO: Load these from some config file
-    public static $APP_PATH = 'app/';
-    public static $WEB_FOLDER = '/';
-    public static $SITE_NAME = '';
-    public static $DATABASE_QUOTE_STYLE = KrisConfig::QuoteStyleMysql;
+    const APP_PATH = 'app/';
+    const WEB_FOLDER = '/krismvc';
 
     // Change Database connections here... TODO: Load these from some config file
-    private static $DB_HOST = '';
-    private static $DB_DATABASE = '';
-    private static $DB_USER = '';
-    private static $DB_PASSWORD = '';
+    const DB_HOST = '';
+    const DB_DATABASE = '';
+    const DB_USER = '';
+    const DB_PASSWORD = '';
+
+    // Quote style
+    public static $DATABASE_QUOTE_STYLE = KrisConfig::QuoteStyleMysql;
+
     private static $DB_CONNECTION;
 
     const DATE_STR = 'l jS \of F Y';
@@ -33,7 +35,7 @@ class KrisConfig
         {
             try
             {
-                static::$DB_CONNECTION = new PDO('mysql:host='.self::$DB_HOST.';dbname='.self::$DB_DATABASE, self::$DB_USER, self::$DB_PASSWORD);
+                static::$DB_CONNECTION = new PDO('mysql:host='.self::DB_HOST.';dbname='.self::DB_DATABASE, self::DB_USER, self::DB_PASSWORD);
             }
             catch (PDOException $e)
             {
@@ -50,7 +52,7 @@ class KrisConfig
 
     public static function Autoload($className)
     {
-        require(static::$APP_PATH.static::$ClassLoader[$className]);
+        require(self::APP_PATH.static::$ClassLoader[$className]);
     }
 
     public static function AddClass($className, $classLocation)
@@ -77,12 +79,12 @@ set_exception_handler('uncaught_exception_handler');
 function uncaught_exception_handler($e) {
   ob_end_clean(); //dump out remaining buffered text
   $vars['message']=$e;
-  die(View::do_fetch(KrisConfig::$APP_PATH.'errors/exception_uncaught.php',$vars));
+  die(View::do_fetch(KrisConfig::APP_PATH.'errors/exception_uncaught.php',$vars));
 }
 
 function custom_error($msg='') {
   $vars['msg']=$msg;
-  die(View::do_fetch(KrisConfig::$APP_PATH.'errors/custom_error.php',$vars));
+  die(View::do_fetch(KrisConfig::APP_PATH.'errors/custom_error.php',$vars));
 }
 */
 
