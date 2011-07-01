67d37af0ddbfaf64e079f713bf707eedae9006ef
Kris Erickson <ksoncan34@yahoo.ca>
2011-07-01 09:03:22 -0700

    Fix includes.

--- index.php
+++ index.php
@@ -16,18 +16,12 @@ require('framework/lib/KrisDB.php');
 require('framework/lib/KrisModel.php');
 require('framework/lib/KrisDBView.php');
 require('framework/lib/KrisView.php');
-
-
-//===============================================
-// Database
-//===============================================
+require('framework/lib/KrisCrudModel.php');
 
 
 //===============================================
 // Autoloading for Business Classes
 //===============================================
-// Assumes Model Classes start with capital letters and Helpers start with lower case letters
-
 function __autoload($className)
 {
     if (KrisConfig::HasClass($className))
@@ -38,7 +32,7 @@ function __autoload($className)
     {
         if (substr($className, -5) == 'Model')
         {
-            require(KrisConfig::APP_PATH . 'models/Generated/' . $className . '.php');
+            require(KrisConfig::APP_PATH . 'models/generated/' . $className . '.php');
         }
         else
         {
