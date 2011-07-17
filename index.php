d108cde6a16b80cba6ebb89f41445094404819fd
Kris Erickson <ksoncan34@yahoo.ca>
2011-07-17 12:28:21 -0700

    Deleting unused files.  Update index.php

--- index.php
+++ index.php
@@ -11,17 +11,25 @@ error_reporting(E_ALL);
 //===============================================
 require('config/KrisConfig.php');
 
-require('framework/lib/KrisController.php');
-require('framework/lib/KrisDB.php');
-require('framework/lib/KrisModel.php');
-require('framework/lib/KrisDBView.php');
-require('framework/lib/KrisView.php');
-require('framework/lib/KrisCrudModel.php');
+require(KrisConfig::FRAMEWORK_DIR.'lib/controller/KrisController.php');
+require(KrisConfig::FRAMEWORK_DIR.'lib/view/KrisView.php');
 
+// This will be loaded as needed...
+KrisConfig::AddClass('KrisDB', KrisConfig::FRAMEWORK_DIR.'/lib/orm/KrisDB.php', true);
+KrisConfig::AddClass('KrisModel', KrisConfig::FRAMEWORK_DIR.'/lib/orm/KrisModel.php', true);
+KrisConfig::AddClass('KrisDBView', KrisConfig::FRAMEWORK_DIR.'/lib/orm/KrisDBView.php', true);
+KrisConfig::AddClass('KrisCrudModel', KrisConfig::FRAMEWORK_DIR.'/lib/orm/KrisCrudModel.php', true);
+KrisConfig::AddClass('HtmlHelpers', KrisConfig::FRAMEWORK_DIR.'/lib/helpers/HtmlHelpers.php', true);
 
-//===============================================
-// Autoloading for Business Classes
-//===============================================
+KrisConfig::AddClass('ImageResizer', KrisConfig::FRAMEWORK_DIR.'/lib/helpers/ImageResizer.php', true);
+KrisConfig::AddClass('NumberHelpers', KrisConfig::FRAMEWORK_DIR.'/lib/helpers/NumberHelpers.php', true);
+
+/**
+ * Autoloading for Business Classes
+ *
+ * @param $className
+ * @return void
+ */
 function __autoload($className)
 {
     if (KrisConfig::HasClass($className))
