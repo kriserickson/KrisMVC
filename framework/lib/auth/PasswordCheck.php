96616cbd881d77d8840e58516e9126b990083075
Kris Erickson <ksoncan34@yahoo.ca>
2011-08-07 15:07:33 -0700

    Made password check an interface so that it can be plugged in with the DI container.

---
+++ framework/lib/auth/PasswordCheck.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Kris
+ * Date: 8/6/11
+ * Time: 1:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+interface PasswordCheck {
+/**
+     * @param string $password
+     * @return string
+     */
+	public function HashPassword($password);
+
+
+    /**
+     * @param string $password
+     * @param string $stored_hash
+     * @return bool
+     */
+	public function CheckPassword($password, $stored_hash);
+}
